af95f7f96126d51d923c9df3bd8462aeca682c18
Bayarto Divasanov <bayarto.divasanov@gmail.com>
2024-06-25 13:48:45 -0400

    commit1

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -2,7 +2,6 @@ import { useState, useRef, useEffect, FC } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { TTabMode } from '@utils-types';
-<<<<<<< HEAD
 import { BurgerIngredientsUI } from '../ui/burger-ingredients';
 import { selectorIngredients } from '../../services/slices/ingredients/index';
 import { useSelector } from '../../services/store/store';
@@ -13,10 +12,6 @@ export const BurgerIngredients: FC = () => {
   const mains = ingredients.filter((item) => item.type === 'main');
   const sauces = ingredients.filter((item) => item.type === 'sauce');
 
-=======
-
-export const BurgerIngredients: FC = () => {
->>>>>>> a54ca2fff2f821f840d691e8e4ed242be00ad31e
   const [currentTab, setCurrentTab] = useState<TTabMode>('bun');
   const titleBunRef = useRef<HTMLHeadingElement>(null);
   const titleMainRef = useRef<HTMLHeadingElement>(null);
@@ -44,10 +39,6 @@ export const BurgerIngredients: FC = () => {
     }
   }, [inViewBuns, inViewFilling, inViewSauces]);
 
-<<<<<<< HEAD
-=======
-  /* В можно лучше: скролл к разделу при клике на таб */
->>>>>>> a54ca2fff2f821f840d691e8e4ed242be00ad31e
   const onTabClick = (tab: string) => {
     setCurrentTab(tab as TTabMode);
     if (tab === 'bun')
@@ -58,7 +49,6 @@ export const BurgerIngredients: FC = () => {
       titleSaucesRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
-<<<<<<< HEAD
   return (
     <BurgerIngredientsUI
       currentTab={currentTab}
@@ -74,23 +64,4 @@ export const BurgerIngredients: FC = () => {
       onTabClick={onTabClick}
     />
   );
-=======
-  // return (
-  //   <BurgerIngredientsUI
-  //     currentTab={currentTab}
-  //     buns={buns}
-  //     mains={mains}
-  //     sauces={sauces}
-  //     titleBunRef={titleBunRef}
-  //     titleMainRef={titleMainRef}
-  //     titleSaucesRef={titleSaucesRef}
-  //     bunsRef={bunsRef}
-  //     mainsRef={mainsRef}
-  //     saucesRef={saucesRef}
-  //     onTabClick={onTabClick}
-  //   />
-  // );
-
-  return null;
->>>>>>> a54ca2fff2f821f840d691e8e4ed242be00ad31e
 };
